8b20865c648bbf53aa58bd0d60291bd47d4c8b6c
Richard <zhu447136076@foxmail.com>
2020-12-30 16:03:52 +0800

    1229

---
+++ former/main.py
@@ -0,0 +1,44 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+from player import *
+from bomb import *
+SCREEN_WIDTH=1000
+SCREEN_HEIGHT=600
+BG_COLOR=pygame.Color(0,0,0)
+NUMBER_OF_PLAYERS=2
+POSITION_OF_PLAYERS=[[50,50],[250,250],[50,250],[250,50]]
+#游戏类
+class MainGame():
+	window=None
+	died=None
+	players=[]
+	def __init__(self):
+		pass
+	def startGame(self):
+		pygame.display.init()
+		self.window=pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+		pygame.display.set_caption('Untitled')
+		self.createplayer()
+		while True:
+			self.window.fill(BG_COLOR)
+			pygame.display.flip()
+			eventList = pygame.event.get()
+			for event in eventList:
+				if event.type == pygame.QUIT:
+					print('感谢支持')
+					exit()
+			self.blitPlayers()
+			pygame.display.update()
+	def createplayer(self):
+		for i in range(0,NUMBER_OF_PLAYERS):
+			self.players.append(player(POSITION_OF_PLAYERS[i][0],POSITION_OF_PLAYERS[i][1]))
+	def blitPlayers(self):
+		if len(self.players)==0:
+			pass
+		else:
+			for i in self.players:
+				self.window.blit(i.images[i.direction],i.rect)
+
+if __name__ == '__main__':
+	MainGame().startGame()
---
+++ properties_panel.py
@@ -0,0 +1,82 @@
+import pygame
+
+
+class PropertiesPanel():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def build_text(content):
+        my_font = pygame.font.SysFont("simsunnsimsun", 25)
+        return my_font.render(content, True, (0, 0, 0))
+
+    @staticmethod
+    def draw_panel(screen, player_id, HP, speed, scope, bombs):
+        text_surfaces = []
+        text_surfaces.append(PropertiesPanel.build_text(str(HP)))
+        text_surfaces.append(PropertiesPanel.build_text(str(speed)))
+        text_surfaces.append(PropertiesPanel.build_text(str(scope)))
+        text_surfaces.append(PropertiesPanel.build_text(str(bombs)))
+        y = 70 if player_id == 1 else 350
+        screen.blit(pygame.image.load(
+            'resources/image/numback_2.png').convert_alpha(), (730, y))
+        for i in range(len(text_surfaces)):
+            screen.blit(text_surfaces[i], (735, y))
+            y += 57
+        pygame.display.flip()
+
+    @staticmethod
+    def draw_items(screen):
+        # pictures = ['resources/image/tool3.png', 'resources/image/tool0.png',
+        #             'resources/image/bomb_center.png', 'resources/image/bomb.png']
+        # 画框
+        screen.blit(pygame.image.load(
+            'resources/image/state.png').convert_alpha(), (600, 0))
+        # screen.blit(pygame.image.load(
+        #     'resources/image/kuang.png').convert_alpha(), (600, 300))
+
+        # # 玩家logo
+        # screen.blit(pygame.image.load(
+        #     'resources/image/player1_down.png').convert_alpha(), (660, 20))
+        # screen.blit(pygame.image.load(
+        #     'resources/image/player2_down.png').convert_alpha(), (660, 320))
+
+        # # 属性logo
+        # y = 80
+        # for i in pictures:
+        #     screen.blit(pygame.image.load(i).convert_alpha(), (640, y))
+        #     y += 40
+
+        # y = 380
+        # for i in pictures:
+        #     screen.blit(pygame.image.load(i).convert_alpha(), (640, y))
+        #     y += 40
+
+    @staticmethod
+    def showHelp(screen):
+        tips_board = pygame.image.load('resources/image/tips_boardhelp2.png')
+        x, y = (80, 260)
+        screen.blit(tips_board, (x, y))
+        # 关闭帮助面板
+        w, h = tips_board.get_size()
+        first_y = 20
+        screen.blit(PropertiesPanel.build_text(
+            '操作说明'), (x + 230, y + first_y))
+        screen.blit(PropertiesPanel.build_text(
+            '玩家1：'), (x + 30, y + first_y + 30))
+        screen.blit(PropertiesPanel.build_text(
+            'W:上 A:左 S:下 D:右 空格:放炸弹'), (x + 30, y + first_y + 30 + 30))
+        # screen.blit(PropertiesPanel.build_text(
+        #     '空格:放炸弹'), (x + 30, y + first_y + 30 + 30 + 30))
+        screen.blit(PropertiesPanel.build_text(
+            '玩家2：'), (x + 30, y + first_y + 30 + 30 + 30))
+        screen.blit(PropertiesPanel.build_text(
+            '↑:上 ←:左 ↓:下 →:右 小键盘0：放炸弹'), (x + 30, y + first_y + 30 + 30 + 30 + 30))
+        # screen.blit(PropertiesPanel.build_text(
+        #     '0：放炸弹'), (x + 30, y + first_y + 30 + 30 + 30 + 30 + 30 + 30))
+        screen.blit(PropertiesPanel.build_text(
+            '制作人：'), (x + 30, y + first_y + 30 + 30 + 30 + 30 + 60))
+        screen.blit(PropertiesPanel.build_text(
+            '柯司博 匡乾 赖若潘 劳马东'), (x + 30, y + first_y + 30 + 30 + 30 + 30 + 60 + 30))
+        return x, y, w
